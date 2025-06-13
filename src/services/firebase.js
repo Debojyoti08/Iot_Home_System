4c88c2859247221f620889076c520d3126dca433
Debojyoti <jiko.das04@gmail.com>
2025-06-13 13:00:07 +0530

    second

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,19 +10,21 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjMO2t-U7dy7PTMEA4JxKQVEuv9xo2LG0",
-  authDomain: "iot-home-system-de90e.firebaseapp.com",
-  projectId: "iot-home-system-de90e",
-  storageBucket: "iot-home-system-de90e.firebasestorage.app",
-  messagingSenderId: "823238731600",
-  appId: "1:823238731600:web:c668a5f456d42b4f8aae55",
-  measurementId: "G-ERVN414N68"
+  apiKey: "AIzaSyD_7v1X0hv3j9GVm-Pa4iGlz8jxUfGEDGE",
+  authDomain: "raspberrypitest-289f4.firebaseapp.com",
+  databaseURL: "https://raspberrypitest-289f4-default-rtdb.firebaseio.com",
+  projectId: "raspberrypitest-289f4",
+  storageBucket: "raspberrypitest-289f4.firebasestorage.app",
+  messagingSenderId: "690728591418",
+  appId: "1:690728591418:web:d4b2f14d3d006b3c53d503",
+  measurementId: "G-JT6P6N0HTG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics1 = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
+
